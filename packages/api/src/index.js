d7dbfd2abdc9f55dd11430ded943f9cd6c664ad9
Piyush <piyush.sharma1420@gmail.com>
2026-05-16 12:53:43 +0530

    implemented zod safe parser

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const authMiddleware = require('./middleware/auth');
 const path = require('path');
+const {TelemetrySchema} = require('./schema/telemetry');
 
 // load environment variables from .env file for all files 
 require('dotenv').config({path: path.join(__dirname, '../../../.env')});
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.get('/health', (req, res) => {
-  res.json({ status: "ok" })
+  res.json({ status: "ok" });
 });
 
+
 app.post('/ingest', authMiddleware, (req, res) => {
-  res.status(202).json({ status: "ingest ok" })
+  // validate the payload shape
+  const result = TelemetrySchema.safeParse(req.body);
+  if(!result.success){
+    return res.status(400).json({error: result.error});
+  }
+  return res.status(202).json({ status: "ingest ok" }); 
+
 });
 
 
